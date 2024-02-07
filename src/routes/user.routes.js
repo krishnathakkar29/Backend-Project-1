07aa0d1b6690d3db1e26c4e7042488dbf7ef94fb
Krishna Thakkar <work.krishnat@gmail.com>
2024-02-07 16:06:20 +0530

    add registe controller and minor fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+  //upload is a middleware , this is the way to wriite middlewares
+  //jo bhi method execute ho rha hai, usse just pehle middleware execute kar do
+  //middleware is used , images lene ke liye
+  upload.fields([
+    {
+      name: "avatar", //frontend mein form mein bhi naam same hona chahiye mostly
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -2,7 +2,9 @@ class ApiResponse {
   constructor(statusCode, message = "Success", data) {
     this.statusCode = statusCode;
     this.data = data;
-    this.message = message
-    this.success = statusCode <400 
+    this.message = message;
+    this.success = statusCode < 400;
   }
 }
+
+export { ApiResponse };
